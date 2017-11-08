aceffdde2d49efe279b8bd979d09935f21777acd
Dane Butler <dbutler@zoll.com>
2017-11-08 15:05:19 -0700

    Implementation of Windows installation/configuration and management of CollectdWinService

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -2,3 +2,16 @@
 # Cookbook:: metricly
 # Recipe:: windows
 #
+
+# Install the MSI package from Metricly/Netuitive
+windows_package 'CollectdWin-x64' do
+  source "#{node['metricly']['windows_agent_msi_url']}"
+  remote_file_attributes ({
+    :path => "#{Chef::Config['file_cache_path']}/CollectdWin-x64.msi"
+    })
+  options "/quiet NETUITIVE_API_KEY=#{node['metricly']['windows_api_key']}"
+end
+
+windows_service 'CollectdWinService (64 bit)' do
+  action [:enable,:start]
+end
