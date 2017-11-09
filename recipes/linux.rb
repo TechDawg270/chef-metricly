a9ca1e8b05d8693269ffd481a950bd98bd4849d5
TechDawg270 <justin.butler270@gmail.com>
2017-11-09 11:46:20 -0700

    Agent installation and configuration for Linux

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -2,3 +2,10 @@
 # Cookbook:: metricly
 # Recipe:: linux
 #
+
+bash 'install_metricly_agent' do
+  code <<-EOH
+    sudo N_APIKEY=#{node['metricly']['linux_api_key']} bash -c "$(curl -Ls http://repos.app.netuitive.com/linux.sh)"
+  EOH
+  not_if { ::File.exist?("/opt/netuitive-agent/conf") }
+end
